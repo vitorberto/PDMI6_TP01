7553d12156a6141f8e177dfb6822c6e5fcf1f1f7
Vitor Berto <berto.vitor@aluno.ifsp.edu.br>
2022-08-28 17:01:49 -0300

    feat: added files

---
+++ PDMI6_TP01/PDMI6_TP01/Credits.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDMI6_TP01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Credits : ContentPage
+    {
+        public Credits()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PDMI6_TP01/PDMI6_TP01/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDMI6_TP01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnOkButtonClicked(object sender, EventArgs args)
+        {
+            string id = idEntry.Text;
+            string password = passEntry.Text;
+
+            if (id == "admin" && password == "@dmin")
+                await DisplayAlert("Sucesso", "Login efetuado com sucesso!", "Ok");
+            else
+                await DisplayAlert("Login não autorizado", "credenciais de acesso incorretas", "Ok");
+        }
+
+        private void OnClearButtonClicked(object sender, EventArgs e)
+        {
+            idEntry.Text = "";
+            passEntry.Text = "";
+
+            idEntry.Focus();
+        }
+
+        private void OnCreditsButtonClicked(object sender, EventArgs e)
+        {
+            ContentPage credits = new Credits();
+            Navigation.PushModalAsync(credits);
+        }
+    }
+}
